78561062a7ef14d77855f7e13a47a7d0e38fe317
m3t0sh1 <metoshi@yandex.ru>
2022-05-10 07:15:42 +0000

    small changes

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 def hello():
     return "Hello world!"
 
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(debug=True,host='0.0.0.0',port=port)
